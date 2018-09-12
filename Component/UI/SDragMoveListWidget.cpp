2b7a3a0189da4e70b9d670dde1cc2066d1edae97
cnscj <cnscj@qq.com>
2018-09-12 21:30:10 +0800

    fix:SDrapMove

--- Component/UI/SDragMoveListWidget.cpp
+++ Component/UI/SDragMoveListWidget.cpp
@@ -14,42 +14,69 @@ SDragMoveListWidget::~SDragMoveListWidget()
 //重写鼠标点击操作.
 void SDragMoveListWidget::mousePressEvent(QMouseEvent *event)
 {
-    //TODO:
     //确保左键拖拽.
-//    if (event->button() == Qt::LeftButton)
-//    {
-//        //先保存拖拽的起点.
-//        m_dragPoint = event->pos();
-//        //保留被拖拽的项.
-//        m_dragItem = this->itemAt(event->pos());
-//    }
-//    //保留原QListWidget部件的鼠标点击操作.
+    if (event->button() == Qt::LeftButton)
+    {
+        //先保存拖拽的起点.
+        m_dragPoint = event->pos();
+        //保留被拖拽的项.
+        m_dragItem = this->itemAt(event->pos());
+    }
+    //保留原QListWidget部件的鼠标点击操作.
     SDragListWidget::mousePressEvent(event);
 }
 
 void SDragMoveListWidget::mouseMoveEvent(QMouseEvent *event)
 {
-    //TODO:
-//    //确保按住左键移动.
-//    if (event->buttons() & Qt::LeftButton)
-//    {
-//        QPoint temp = event->pos() - m_dragPoint;
-//        //只有这个长度大于默认的距离,才会被系统认为是形成拖拽的操作.
-//        if (temp.manhattanLength() > QApplication::startDragDistance())
-//        {
-//            QDrag *drag = new QDrag(this);
-//            QMimeData *mimeData = new QMimeData;
-//            mimeData->setText(m_dragItem->text());
-//            drag->setMimeData(mimeData);
-//            auto action = drag->exec(Qt::CopyAction | Qt::MoveAction);
+    //确保按住左键移动.
+    if (event->buttons() & Qt::LeftButton)
+    {
+        QPoint temp = event->pos() - m_dragPoint;
+        //只有这个长度大于默认的距离,才会被系统认为是形成拖拽的操作.
+        if (temp.manhattanLength() > QApplication::startDragDistance())
+        {
+            QDrag *drag = new QDrag(this);
+            QMimeData *mimeData = new QMimeData;
+            mimeData->setText(m_dragItem->text());
+            drag->setMimeData(mimeData);
+            auto action = drag->exec(Qt::MoveAction);
 
-//            if (action == (Qt::CopyAction) || (action == Qt::MoveAction))
+//            if (action == Qt::MoveAction)
 //            {
 //                //当成功拖拽后，删除拖拽项.
-//                auto i = this->takeItem(this->row(m_dragItem));
-//                delete i;
+////                auto i = this->takeItem(this->row(m_dragItem));
+////                delete i;
 //            }
-//        }
-//    }
+        }
+    }
     SDragListWidget::mouseMoveEvent(event);
+}
+
+
+void SDragMoveListWidget::dragEnterEvent(QDragEnterEvent *event)
+{
+    SDragMoveListWidget *source = (SDragMoveListWidget *)((void *)(event->source()));
+    if (source && source == this)
+    {
+        qDebug("自己的");
+        auto item = this->itemAt(event->pos());//找到当前鼠标位置在部件中的项.
+        //TODO:交换2项
+//        auto i = this->takeItem(this->row(m_dragItem));
+//        delete i;
+
+        event->accept();
+    }
+    else
+    {
+        qDebug("从别的地方来的");
+        SDragListWidget::dragEnterEvent(event);
+    }
+}
+void SDragMoveListWidget::dragMoveEvent(QDragMoveEvent *event)
+{
+    SDragListWidget::dragMoveEvent(event);
+}
+void SDragMoveListWidget::dropEvent(QDropEvent *event)
+{
+    SDragListWidget::dropEvent(event);
 }
--- Component/UI/SDragMoveListWidget.h
+++ Component/UI/SDragMoveListWidget.h
@@ -12,6 +12,10 @@ protected:
     //重写下面两个函数来完成从该部件中拖拽出去的操作.
     virtual void mousePressEvent(QMouseEvent *event);
     virtual void mouseMoveEvent(QMouseEvent *event);
+
+    virtual void dragEnterEvent(QDragEnterEvent *event);        //拖入时
+    virtual void dragMoveEvent(QDragMoveEvent *event);          //拖动时
+    virtual void dropEvent(QDropEvent *event);                  //拖完时
 private:
     QPoint                              m_dragPoint;        //记录拖拽的起点.
     QListWidgetItem                     *m_dragItem;        //记录被拖拽的项.
